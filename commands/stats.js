600663a86a473c92f29004b4744e9692706c1c5c
gavintjhxx <gavintjhx@gmail.com>
2022-03-22 22:56:44 +0800

    [ADD] More details to stats command

--- commands/stats.js
+++ commands/stats.js
@@ -1,18 +1,29 @@
+const osu = require("node-os-utils");
 const { version } = require("discord.js");
 const { codeBlock } = require("@discordjs/builders");
 const { DurationFormatter } = require("@sapphire/time-utilities");
 const durationFormatter = new DurationFormatter();
 
-exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const duration = durationFormatter.format(client.uptime);
+  const cpu = osu.cpu;
+  const mem = osu.mem;
+  const drive = osu.drive;
+  const cpuUsage = await cpu.usage();
+  const { totalGb, usedGb, freeGb, usedPercentage, freePercentage } = await drive.info();
+  const { totalMemMb, usedMemMb, freeMemMb, freeMemPercentage } = await mem.info();
+
   const stats = codeBlock("asciidoc", `= STATISTICS =
-  • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-  • Uptime     :: ${duration}
-  • Users      :: ${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b).toLocaleString()}
-  • Servers    :: ${client.guilds.cache.size.toLocaleString()}
-  • Channels   :: ${client.channels.cache.size.toLocaleString()}
-  • Discord.js :: v${version}
-  • Node       :: ${process.version}`);
+  • CPU Usage (%)  :: ${cpu.count()} cores | ${cpuUsage}%
+  • Mem Usage      :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+  • Bot Mem Usage  :: ${usedMemMb}MB (${100 - freeMemPercentage}) / ${totalMemMb}MB (100%) | ${freeMemMb}MB (${freeMemPercentage}) free
+  • SSD Usage      :: ${usedGb}GB (${usedPercentage}%) / ${totalGb}GB (100%) | ${freeGb}GB (${freePercentage}) free
+  • Uptime         :: ${duration}
+  • Users          :: ${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b).toLocaleString()}
+  • Servers        :: ${client.guilds.cache.size.toLocaleString()}
+  • Channels       :: ${client.channels.cache.size.toLocaleString()}
+  • Discord.js     :: v${version}
+  • Node           :: ${process.version}`);
   message.channel.send(stats);
 };
 
