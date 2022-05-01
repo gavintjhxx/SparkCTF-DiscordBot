50be1f1506f63b86235dfff62a1405779b5d4b23
gavintjhxx <gavintjhx@gmail.com>
2022-05-01 18:39:36 +0800

    [NEW] Emoji logs

---
+++ events/emojiUpdate.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+const guildSettings = require("../models/guildSettings");
+const { compareObjects } = require("../modules/functions");
+
+module.exports = async (client, oldEmoji, newEmoji) => {
+	const guildID = newEmoji.guild.id;
+	const settings = await guildSettings.findOne({ guildID: guildID });
+	const logEnabled = settings.logs.EMOJI_UPDATE.enabled;
+	const logChannelID = settings.logs.EMOJI_UPDATE.logChannelID;
+	if (!logEnabled || !logChannelID) return;
+
+	// Audit Log Stuff
+	const fetchedLogs = await newEmoji.guild.fetchAuditLogs({
+		limit: 1,
+		type: "EMOJI_UPDATE",
+	});
+	const firstLog = fetchedLogs.entries.first();
+	let executor;
+	firstLog ? { executor } = firstLog : executor = "(Unknown)";
+
+	// Get changes in emoji objects
+	const changes = compareObjects(oldEmoji, newEmoji);
+	// console.log(changes);
+	const logEmbed = new MessageEmbed()
+		.setColor("#FFA700")
+		.setTitle("Emoji Updated")
+		.setDescription(`**Executor:** ${executor.tag} (${executor.id})\n**Emoji:** ${newEmoji.name} (${newEmoji.id})\n**Before:** \`\`\`js\n${changes}\`\`\``)
+		.setThumbnail(newEmoji.url)
+		.setTimestamp();
+	client.channels.cache.get(logChannelID).send({ embeds: [ logEmbed ]});
+    
+};
