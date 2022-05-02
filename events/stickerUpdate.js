2a860f0eacae0d931b9d488a51e18ddec8f04f18
gavintjhxx <gavintjhx@gmail.com>
2022-05-02 15:12:29 +0800

    [FEAT/event] sticker events (create, delete, update)

---
+++ events/stickerUpdate.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+const guildSettings = require("../models/guildSettings");
+const { compareObjects } = require("../modules/functions");
+
+module.exports = async (client, oldSticker, newSticker) => {
+	const guildID = newSticker.guild.id;
+	const settings = await guildSettings.findOne({ guildID: guildID });
+	const logEnabled = settings.logs.STICKER_UPDATE.enabled;
+	const logChannelID = settings.logs.STICKER_UPDATE.logChannelID;
+	if (!logEnabled || !logChannelID) return;
+
+	// Audit Log Stuff
+	const fetchedLogs = await newSticker.guild.fetchAuditLogs({
+		limit: 1,
+		type: "STICKER_UPDATE",
+	});
+	const firstLog = fetchedLogs.entries.first();
+	let executor;
+	firstLog ? { executor } = firstLog : executor = "(Unknown)";
+
+	// Get changes in emoji objects
+	const changes = compareObjects(oldSticker, newSticker);
+	// console.log(changes);
+	const logEmbed = new MessageEmbed()
+		.setColor("#FFA700")
+		.setTitle("Sticker Updated")
+		.setDescription(`**Executor:** ${executor.tag} (${executor.id})\n**Emoji:** ${newSticker.name} (${newSticker.id})\n**Before:** \`\`\`js\n${changes}\`\`\``)
+		.setThumbnail(newSticker.url)
+		.setTimestamp();
+	client.channels.cache.get(logChannelID).send({ embeds: [ logEmbed ]});
+    
+};
