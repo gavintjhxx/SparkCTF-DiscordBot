b6f332c8b0b953a3f39f3027a0521041298bc1d9
gavintjhxx <gavintjhx@gmail.com>
2022-05-18 13:17:49 +0800

    [change/event] Remove unnecessary audit stuff

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,22 +8,13 @@ module.exports = async (client, guildMember) => {
 	const logChannelID = settings.logs.GUILD_MEMBER_ADD.logChannelID;
 	if (!logEnabled || !logChannelID) return;
 
-	// Audit Log Stuff
-	const fetchedLogs = await guildMember.fetchAuditLogs({
-		limit: 1,
-		type: "GUILD_MEMBER_ADD",
-	});
-	const firstLog = fetchedLogs.entries.first();
-	let executor;
-	firstLog ? { executor } = firstLog : executor = "(Unknown)";
-
 	const user = `${guildMember.user.tag} (${guildMember.user.id})`;
 	const createdAt = guildMember.user.createdAt;
 	
 	const logEmbed = new MessageEmbed()
 		.setColor("#FFA700")
 		.setTitle("User Joined")
-		.setDescription(`**Executor:** ${executor.tag} (${executor.id})\n**User:** ${user}\n**Account Created At:** ${createdAt}`)
+		.setDescription(`**User:** ${user}\n**Account Created At:** ${createdAt}`)
 		.setThumbnail(guildMember.user.avatarURL())
 		.setTimestamp();
 	client.channels.cache.get(logChannelID).send({ embeds: [ logEmbed ]});
