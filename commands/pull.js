3dd3877bf7305c1d64319e762775d0a7e2830578
gavintjhxx <gavintjhx@gmail.com>
2022-11-02 20:25:52 +0800

    [NEW/command] New pull command, better and faster deployment of new features *and bugs* finally

---
+++ commands/pull.js
@@ -0,0 +1,28 @@
+const { exec } = require("child_process");
+const { getGuildDB, defaultDB, promptFailureEmbed, promptSuccessEmbed } = require("../modules/functions");
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+	const settings = await getGuildDB(message.guild) ? await getGuildDB(message.guild) : await defaultDB(message.guild);
+	// command goes here
+	await promptSuccessEmbed(message, "***Pulling from master branch...***");
+	exec("git pull origin master", async (error, stdout) => {
+		if (error) return promptFailureEmbed(message, "```js\n" + error + "```");
+		return promptSuccessEmbed(message, `***Pulled from master branch.***\n\`\`\`js\n${stdout}\`\`\`\nA restart of the bot is required to apply the changes. (\`${settings.prefix}reboot\`)`);
+	});
+};
+
+// Set guildOnly to true if you want it to be available on guilds only.
+// Otherwise false is global.
+exports.conf = {
+	enabled: true,
+	guildOnly: true,
+	aliases: [],
+	permLevel: "Bot Owner"
+};
+
+exports.help = {
+	name: "pull",
+	category: "System",
+	description: "Pulls the latest updates from the github repository.",
+	usage: "pull"
+};
