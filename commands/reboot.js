c548b5b39e773013dee54f20ea2a7a8fb8bc81e2
gavintjhxx <gavintjhx@gmail.com>
2022-11-02 20:34:24 +0800

    [FIX/command] remove caching stuff since the restart involves theentire service cache being deleted

--- commands/reboot.js
+++ commands/reboot.js
@@ -3,14 +3,7 @@ const { getGuildDB, defaultDB, promptFailureEmbed, promptSuccessEmbed } = requir
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
 	const settings = await getGuildDB(message.guild) ? await getGuildDB(message.guild) : await defaultDB(message.guild);
-	await message.channel.send("Bot is shutting down.");
-	await Promise.all(client.container.commands.map(cmd => {
-		// the path is relative to the *current folder*, so just ./filename.js
-		delete require.cache[require.resolve(`./${cmd.help.name}.js`)];
-		// We also need to delete and reload the command from the container.commands Enmap
-		client.container.commands.delete(cmd.help.name);
-	}));
-	
+	await promptSuccessEmbed(message, "***Restarting...***```js\nsudo systemctl restart zlotherino```");
 	exec("sudo systemctl restart zlotherino.service", async (error, stdout) => {
 		if (error) return promptFailureEmbed(message, "```js\n" + error + "```");
 		return promptSuccessEmbed(message, `***Pulled from master branch.***\n\`\`\`js\n${stdout}\`\`\`\nA restart of the bot is required to apply the changes. (\`${settings.prefix}reboot\`)`);
