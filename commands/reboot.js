124b0b5048c6c99bfa65db48ee61c2e112625264
gavintjhxx <gavintjhx@gmail.com>
2022-11-02 20:37:40 +0800

    [CHANGE/command] reboot command | remove even more unnecessary messages

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,12 +1,10 @@
 const { exec } = require("child_process");
-const { getGuildDB, defaultDB, promptFailureEmbed, promptSuccessEmbed } = require("../modules/functions");
+const { promptFailureEmbed, promptSuccessEmbed } = require("../modules/functions");
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-	const settings = await getGuildDB(message.guild) ? await getGuildDB(message.guild) : await defaultDB(message.guild);
-	await promptSuccessEmbed(message, "***Restarting...***```js\nsudo systemctl restart zlotherino```");
-	exec("sudo systemctl restart zlotherino.service", async (error, stdout) => {
+	await promptSuccessEmbed(message, "***Restarting...***");
+	exec("sudo systemctl restart zlotherino.service", async (error) => {
 		if (error) return promptFailureEmbed(message, "```js\n" + error + "```");
-		return promptSuccessEmbed(message, `***Pulled from master branch.***\n\`\`\`js\n${stdout}\`\`\`\nA restart of the bot is required to apply the changes. (\`${settings.prefix}reboot\`)`);
 	});
 };
 
